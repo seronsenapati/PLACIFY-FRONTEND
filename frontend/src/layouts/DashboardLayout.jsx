9f49799c71b14026a1f83a7c60443e397f49c710
seronsenapati <seronsenapati@gmail.com>
2025-08-18 19:50:44 +0530

    Fix the dashboard

--- frontend/src/layouts/DashboardLayout.jsx
+++ frontend/src/layouts/DashboardLayout.jsx
@@ -59,7 +59,7 @@ export default function DashboardLayout({ children }) {
       {/* Main content */}
       <div className="flex-1 flex flex-col">
         {/* Top Navbar */}
-        <header className="flex justify-between items-center bg-gradient-to-r from-white/5 to-transparent backdrop-blur-md border-b  border-dashed border-white/20 px-6 py-4">
+        <header className="flex justify-between items-center border-b border-dashed border-white/20 px-6 py-4">
           <h1 className="text-3xl">
             {username ? `Welcome back, ${username}.` : "Welcome to Dashboard"}
           </h1>
